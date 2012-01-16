6dc95bb999d805773438f1055dbcbbde5e73527e
jonesbb <jonesbb@gmail.com>
2012-01-16 15:39:21 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musicbachs::Application.config.secret_token = 'd39dff295cd0b295ab80dd8cd2ba1218d2dbd0d2581228fea8b43a9ce28ae5c1ec3571d0203d15259d7bcb53d81c6727187ab98b9120bb97d093a44459a740a9'
