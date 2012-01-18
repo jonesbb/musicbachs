6b0d01e3bc06143450164f5569f44531707dab5a
jonesbb <jonesbb@gmail.com>
2012-01-18 11:36:00 -0800

    got initial search form sort of working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,10 +13,23 @@ class PagesController < ApplicationController
 
   def search
     @title = "Search"
-  end
-
-  def search_artists
+    
     artist_search_text = params['myform']['artistext']
     #do your stuff with comments_from_form here
+    
+    results = MetaSpotify::Artist.search(artist_search_text)
+
+    items_per_page = results[:items_per_page]
+    starting_item  = results[:start_index]
+    
+    #Find out how many pages of results there are
+    quantity = results[:total_results]
+    pages = (quantity.to_f/items_per_page).ceil
+    
+    @fr = results.keys
+    @qua = quantity
+    @temp = results[:artists].each{|i| puts i.name}
+
   end
+
 end
--- app/controllers/spotify_search_controller.rb
+++ app/controllers/spotify_search_controller.rb
@@ -1,5 +1,7 @@
 class SpotifySearchController < ApplicationController
   def artist
+    artist_search_text = params['myform']['artistext']
+    #do your stuff with comments_from_form here
   end
 
   def album
