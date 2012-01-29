7d98bf79d136c525d25b9054f6e8cb26db797480
jonesbb <jonesbb@gmail.com>
2012-01-28 18:09:06 -0800

    got search and format into table

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,22 +14,35 @@ class PagesController < ApplicationController
   def search
     @title = "Search"
     
+    #pull the text from the search box
     artist_search_text = params['myform']['artistext']
-    #do your stuff with comments_from_form here
-    
-    results = MetaSpotify::Artist.search(artist_search_text)
-
-    items_per_page = results[:items_per_page]
-    starting_item  = results[:start_index]
-    
-    #Find out how many pages of results there are
-    quantity = results[:total_results]
-    pages = (quantity.to_f/items_per_page).ceil
-    
-    @fr = results.keys
-    @qua = quantity
-    @temp = results[:artists].each{|i| puts i.name}
-
+    @a_srch_txt = artist_search_text
+    #add some check for artist_search_text to be non-empty
+    if artist_search_text.blank?
+      @no_search = "empty search"
+    else
+      # Call the metaspotify wrapper
+      # :query, :items_per_page, :start_index, :total_results
+      results = MetaSpotify::Artist.search(artist_search_text)  
+  
+      items_per_page = results[:items_per_page]
+      starting_item  = results[:start_index]
+      
+      #Find out how many pages of results there are
+      quantity = results[:total_results]
+      pages = (quantity.to_f/items_per_page).ceil
+      
+      @fr = results.keys
+      @qua = quantity    
+      artist_results = results[:artists]   # :name, :popularity, :uri
+  
+      @a_var = artist_results
+  
+      ###
+      #if temp2.is_a?(Array)
+       # @vari = temp2.length
+      #end
+    end
   end
 
 end
